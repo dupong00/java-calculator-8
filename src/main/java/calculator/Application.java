8fcfe443d897670359b950a3738b7241ae29b975
dupong00 <tjdgns9954@gmail.com>
2025-10-17 23:16:18 +0900

    feat : 메인 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        StringParser sp = new StringParser(str);
+        List<Integer> a = sp.getNumbers();
+
+        AddCalculator ac = new AddCalculator(a);
+        System.out.println("결과 : " + ac.getSum());
     }
 }
